dfe2e0a307ac5124369e35b065b584b5c5418b6b
Lukas Borggren <lukas.borggren@gmail.com>
2020-06-30 08:37:22 +0200

    Continued work on evaluating category similarity

--- embeddings.py
+++ embeddings.py
@@ -12,34 +12,45 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from help_functions import write_df_to_file, read_df_from_file
 import pickle
 
+
 def create_embedding(sentence):
     input_ids = torch.tensor(tokenizer.encode(sentence)).unsqueeze(0)  # Batch size 1
     outputs = model(input_ids)
-    last_hidden_states = outputs[0]  # The last hidden-state is the first element of the output tuple
+    last_hidden_states = outputs[
+        0
+    ]  # The last hidden-state is the first element of the output tuple
     return last_hidden_states
 
 
 def initial_similarity(categories_df):
-    categories_df['embedding'] = categories_df['category'].apply(lambda x: create_embedding(x))
-    print('Embeddings created')
+    categories_df["embedding"] = categories_df["category"].apply(
+        lambda x: create_embedding(x)
+    )
+    print("Embeddings created")
     copy_df = categories_df.copy()
 
     cnt = 0
     for i in categories_df.index:
-        emb_i = categories_df['embedding'][i]
-        for j in copy_df.index[i+1:]:
-            emb_j = categories_df['embedding'][j]
+        emb_i = categories_df["embedding"][i]
+        for j in copy_df.index[i + 1 :]:
+            emb_j = categories_df["embedding"][j]
             sim = cos(emb_i, emb_j)
             if sim.item() > 0.995:
                 cnt += 1
                 print(i, j)
-                print('Merged categories', cnt, sim.item(), categories_df['category'][i], categories_df['category'][j])
+                print(
+                    "Merged categories",
+                    cnt,
+                    sim.item(),
+                    categories_df["category"][i],
+                    categories_df["category"][j],
+                )
 
 
 def TFIDF_article_similarity():
-    articles = get_articles('data/articles_small.json')
-    stopwords = stopwords.words('swedish')
-    corpus = [article['content_text'] for article in articles]
+    articles = get_articles("data/articles_small.json")
+    stopwords = stopwords.words("swedish")
+    corpus = [article["content_text"] for article in articles]
     vect = TfidfVectorizer(min_df=1, stop_words=stopwords)
     tfidf = vect.fit_transform(corpus)
     pairwise_similarity = tfidf * tfidf.T
@@ -48,17 +59,18 @@ def TFIDF_article_similarity():
 
 def BERT_article_similarity():
 
-    articles = get_articles('data/articles_small.json')
-    stopwords = stopwords.words('swedish')
+    articles = get_articles("data/articles_small.json")
+    stopwords = stopwords.words("swedish")
     embeddings = []
     for article in articles:
         temp = []
-        sentences = article['content_text'].replace('\n\n', '.').split('.')
+        sentences = article["content_text"].replace("\n\n", ".").split(".")
         for sentence in sentences:
-            if not sentence.strip(): continue
+            if not sentence.strip():
+                continue
             words = sentence.split()
             print(tokenizer.encode(sentence))
-            #print([word for word in words if word not in stopwords ])
+            # print([word for word in words if word not in stopwords ])
             ok_ind_w = [i for i in range(len(words)) if words[i] not in stopwords]
             token_lens = [len(tokenizer.encode(word)) - 2 for word in words]
             ok_ind_t = []
@@ -67,22 +79,23 @@ def BERT_article_similarity():
                 ok_ind_t += [prev + i for i in range(0, token_lens[i])]
             ok_ind_t = [i + 1 for i in ok_ind_t]
             try:
-                embedding = create_embedding(sentence.strip() + '.')
+                embedding = create_embedding(sentence.strip() + ".")
                 embedding = embedding[:, ok_ind_t, :]
                 temp += [embedding]
             except IndexError:  # 1541 max length sentence
                 print(sentence)
                 continue
         embeddings += [temp]
-        print('-' * 100)
-    print(len(embeddings), 'article embeddings created!')
+        print("-" * 100)
+    print(len(embeddings), "article embeddings created!")
 
     all_sims = []
     for i, art_i in enumerate(embeddings):
         art_sims = []
         for j, art_j in enumerate(embeddings):
-            print('Now comparing article', i, 'with article', j,'…')
-            if i == j: continue
+            print("Now comparing article", i, "with article", j, "…")
+            if i == j:
+                continue
             sen_sims = []
             for sen_i in art_i:
                 tok_sims = []
@@ -92,67 +105,111 @@ def BERT_article_similarity():
                             sim = cos(sen_i[:, tok_i, :], sen_j[:, tok_j, :]).item()
                             tok_sims += [sim]
                 sen_sims += [max(tok_sims)]
-            art_sims += [(sum(sen_sims)/len(sen_sims))**2]
+            art_sims += [(sum(sen_sims) / len(sen_sims)) ** 2]
         all_sims += [art_sims]
 
-    print('[hockey, hockey, börs, börs]')
+    print("[hockey, hockey, börs, börs]")
     for sim in all_sims:
         print(sim)
 
 
 def create_entity_embeddings(categories):
-    entities = read_df_from_file('data/merged_entities_df.jsonl')
-    entities['embedding'] = entities['word'].apply(lambda x: create_embedding(x))
+    entities = read_df_from_file("data/merged_entities_df.jsonl")
+    entities["embedding"] = entities["word"].apply(lambda x: create_embedding(x))
     print(entities)
 
     embeddings_list = []
     for ind in categories.index:
         category_embeddings = []
-        for entity in categories['entities'][ind]:
-            category_embeddings += [entities[entities['word'].apply(lambda x: x == entity[1])]['embedding']]
+        for entity in categories["entities"][ind]:
+            category_embeddings += [
+                entities[entities["word"].apply(lambda x: x == entity[1])]["embedding"]
+            ]
         embeddings_list += [category_embeddings]
 
-    with open('data/entity_embeddings.pickle','wb') as f:
+    with open("data/entity_embeddings.pickle", "wb") as f:
         pickle.dump(embeddings_list, f)
 
-tokenizer = BertTokenizer.from_pretrained('KB/bert-base-swedish-cased-ner')
-model = BertModel.from_pretrained('KB/bert-base-swedish-cased-ner')
+
+def rescale(vs):
+    mn = min(vs)
+    mx = max(vs)
+    denominator = mx - mn
+    return [(v - mn) / denominator for v in vs]
+
+
+def filter_out_infrequent(df):
+    df = df[df["no_uses"] > 2]
+    return df  # .reset_index(drop=True)
+
+
+def calculate_entity_weight(df, i1, i2):
+    return df["entities"][i1][i2][0] / df["tot_no_entities"][i1]
+
+
+tokenizer = BertTokenizer.from_pretrained("KB/bert-base-swedish-cased-ner")
+model = BertModel.from_pretrained("KB/bert-base-swedish-cased-ner")
 cos = nn.CosineSimilarity()
 
-categories = read_df_from_file('data/categories_df.jsonl')
-#create_entity_embeddings(categories)
+categories = read_df_from_file("data/categories_df.jsonl")
+# create_entity_embeddings(categories)
 
-print('Unpickling…')
-with open('data/entity_embeddings.pickle','rb') as f:
+print("Unpickling…")
+with open("data/entity_embeddings.pickle", "rb") as f:
     embeddings = pickle.load(f)
-print('Unpickled!')
+print("Unpickled!")
 
+categories = filter_out_infrequent(categories)
+embeddings = list(map(embeddings.__getitem__, categories.index.tolist()))
+categories = categories.reset_index(drop=True)
+write_df_to_file(categories, "data/filtered_cats_df.jsonl")
 no_categories = categories.shape[0]
-sim_matrix = np.empty([no_categories, no_categories])
-all_sim = []
-for i1 in range(0,2):
-    cat_sim = []
+sim_matrix = np.zeros([no_categories, no_categories])
+
+for i1 in categories.index:
+    cat_sim = [None] * len(categories.index)
     for j1 in categories.index:
-        print('Comparing category', i1, 'with category', j1, '…')
-        ent_sim = []
-        for i2 in range(0, len(categories['entities'][i1])):
-            w = categories['entities'][i1][i2][0] / categories['tot_no_entities'][i1]
+        len_i = len(categories["entities"][i1])
+        len_j = len(categories["entities"][j1])
+        print("Comparing category", i1, "with category", j1, "…")
+        ent_sim = [None] * len_i * len_j
+        for i2 in range(0, len(categories["entities"][i1])):
+            w1 = calculate_entity_weight(categories, i1, i2)
             emb_i = embeddings[i1][i2].item()
-            for j2 in range(0, len(categories['entities'][j1])):
+            for j2 in range(0, len(categories["entities"][j1])):
+                # w2 = calculate_entity_weight(categories, j1, j2)
                 emb_j = embeddings[j1][j2].item()
                 smallest = range(0, min(emb_i.shape[1], emb_j.shape[1]))
-                ent_sim += [cos(emb_i[:, smallest, :], emb_j[:, smallest, :]).mean().item() * w]
-                #print(ent_sim[-1])
-        cat_sim += [sum(ent_sim) / len(ent_sim)] if ent_sim else [0]
-    sim_matrix[i1] = cat_sim
+                sim = cos(emb_i[:, smallest, :], emb_j[:, smallest, :])
+                # print(sim.mean())
+                curr_ind = i2 * len_j + j2
+                ent_sim[curr_ind] = sim.mean().item() * w1
+        cat_sim[j1] = sum(ent_sim) / len(ent_sim) if ent_sim else 0
+    sim_matrix[i1] = rescale(cat_sim)
 
-max_val = np.amax(sim_matrix, axis=1)
-max_ind = np.argmax(sim_matrix, axis=1)
-print(sim_matrix)
-print(max_val)
-print(max_ind)
+with open("data/category_similarity_matrix.pickle", "wb") as f:
+    pickle.dump(sim_matrix, f)
 
+# with open("data/category_similarity_matrix.pickle", "rb") as f:
+#     sim_matrix = pickle.load(f)
 
+# max_val = np.fliplr(np.partition(sim_matrix, -3)[:, -3:])
+# max_ind = np.fliplr(np.argpartition(sim_matrix, -5)[:, -5:])
+
+# for ind in categories.index[0:1]:
+#     maxs = [
+#         (categories["category"][n], categories["no_unique_entities"][n])
+#         for n in max_ind[ind]
+#     ]
+#     print(categories["category"][ind], "has largest similarity with", maxs)
+
+# TODO: Frekeventa entiteter har FÖR LITE impact på den slutigiltiga likheten
+"""Hypoteser:   1. viktningen är icke-representativ
+                2. mean av tensor är ett dåligt sätt att erhålla en skalär för entiteslikhet
+                3. mean av entititlikheter är ett dåligt sätt att erhålla den slutgiltiga artikellikheten"""
 
 # TODO: en viss andel av entitetslikheter bör överstiga något tröskelvärde
 # TODO: dela upp utifrån entitetstyper
+
+# Using two weights:
+# AB Volvo has largest similarity with [('Soundcloud', 3), ('Drograttfylleri', 6), ('Omsorg', 8), ('Fordonsbrand', 10), ('Kry', 8)]
--- entity_extraction.py
+++ entity_extraction.py
@@ -1,11 +1,11 @@
-from transformers import pipeline, BertTokenizer
+from transformers import pipeline
 import re
 import json
 import jsonlines
 
 
 def get_articles(articles_file):
-    with open(articles_file, 'r') as f_handle:
+    with open(articles_file, "r") as f_handle:
         articles_arr = []
         articles = json.load(f_handle)
         for article in articles:
@@ -14,91 +14,104 @@ def get_articles(articles_file):
 
 
 def handle_ambiguity(current, previous):
-    if not isinstance(previous['entity'], list): previous['entity'] = [previous['entity']]
-    previous['entity'] += [{'type': current['entity'], 'subtoken': current['word']}]
+    if not isinstance(previous["entity"], list):
+        previous["entity"] = [previous["entity"]]
+    previous["entity"] += [{"type": current["entity"], "subtoken": current["word"]}]
 
 
 def format_entities(raw_entities, text):
-    
-    is_char = lambda x: x in ['-', ',', '\"', '\'', '’', '#']
-    
+
+    is_char = lambda x: x in ["-", ",", '"', "'", "’", "#"]
+
     formatted_entities = []
     no_subtokens = 1
-    
+
     for i, current in enumerate(raw_entities):
-        
-        if '[UNK]' in current['word']: current['word'] = current['word'].replace('[UNK]', '').strip()
-        if not current['word']:
-            current['entity'] = 'NA'
+
+        if "[UNK]" in current["word"]:
+            current["word"] = current["word"].replace("[UNK]", "").strip()
+        if not current["word"]:
+            current["entity"] = "NA"
             continue
 
         if formatted_entities:
             previous = formatted_entities[-1]
-            adjacent = previous['index'] == current['index'] - 1
-            same_entity = previous['entity'] == current['entity']
-        
+            adjacent = previous["index"] == current["index"] - 1
+            same_entity = previous["entity"] == current["entity"]
+
         end_of_article = i + 1 >= len(raw_entities)
         if not end_of_article:
             subseq = raw_entities[i + 1]
-            end_of_sentence = subseq['index'] < current['index']
+            end_of_sentence = subseq["index"] < current["index"]
 
-            if current['word'] == ':' and subseq['word'] == 's':
-                subseq['word'] = '[UNK]'
+            if current["word"] == ":" and subseq["word"] == "s":
+                subseq["word"] = "[UNK]"
                 no_subtokens = 1
                 continue
-            
-        if formatted_entities and current['word'].startswith('##'):
+
+        if formatted_entities and current["word"].startswith("##"):
             if adjacent:
-                previous['index'] = current['index']
-                previous['word'] += current['word'][2:]
-                previous['score'][-1] += current['score']
+                previous["index"] = current["index"]
+                previous["word"] += current["word"][2:]
+                previous["score"][-1] += current["score"]
                 no_subtokens += 1
-                if not same_entity: handle_ambiguity(current, previous)
+                if not same_entity:
+                    handle_ambiguity(current, previous)
             else:
-                current['entity'] = 'NA'
+                current["entity"] = "NA"
         elif formatted_entities and adjacent:
-            previous['index'] = current['index']
-            if (is_char(current['word']) or is_char(previous['word'][-1])) and not current['word'] == 'och':
-                previous['word'] += current['word']
+            previous["index"] = current["index"]
+            if (
+                is_char(current["word"]) or is_char(previous["word"][-1])
+            ) and not current["word"] == "och":
+                previous["word"] += current["word"]
             else:
-                previous['word'] += ' ' + current['word']
-            previous['score'] += [current['score']]
+                previous["word"] += " " + current["word"]
+            previous["score"] += [current["score"]]
             no_subtokens = 1
-            if not same_entity: handle_ambiguity(current, previous)
+            if not same_entity:
+                handle_ambiguity(current, previous)
         else:
-            current['score'] = [current['score']]
+            current["score"] = [current["score"]]
             formatted_entities += [current.copy()]
             no_subtokens = 1
 
         if not end_of_article:
-            end_of_entity_series = no_subtokens > 1 and not subseq['word'].startswith('##')
+            end_of_entity_series = no_subtokens > 1 and not subseq["word"].startswith(
+                "##"
+            )
 
         if (end_of_article or end_of_sentence) or end_of_entity_series:
-            formatted_entities[-1]['score'][-1] = formatted_entities[-1]['score'][-1] / no_subtokens
+            formatted_entities[-1]["score"][-1] = (
+                formatted_entities[-1]["score"][-1] / no_subtokens
+            )
             no_subtokens = 1
     return formatted_entities
-    
 
-nlp = pipeline('ner', model='KB/bert-base-swedish-cased-ner', tokenizer='KB/bert-base-swedish-cased-ner')
-tokenizer = BertTokenizer.from_pretrained('KB/bert-base-swedish-cased-ner')
-articles = get_articles('data/articles.json')
+
+nlp = pipeline(
+    "ner",
+    model="KB/bert-base-swedish-cased-ner",
+    tokenizer="KB/bert-base-swedish-cased-ner",
+)
+articles = get_articles("data/articles.json")
 
 article_cnt = 0
 json_output = []
 failed_articles = []
 
 for i, article in enumerate(articles):
-    article_text = article['content_text']
-    sentences = article_text.replace('\n\n', '.').split('.')
+    article_text = article["content_text"]
+    sentences = article_text.replace("\n\n", ".").split(".")
     article_entities = []
     for sentence in sentences:
-        if re.search('<.*>', sentence):
+        if re.search("<.*>", sentence):
             failed_articles += [article]
             break
         elif not sentence.strip():
             continue
         try:
-            input_sentence = sentence.strip() + '.'
+            input_sentence = sentence.strip() + "."
             entities = nlp(input_sentence)
             article_entities += entities
         except IndexError:  # 1541 max length sentence
@@ -107,21 +120,21 @@ for i, article in enumerate(articles):
     # for entity in article_entities:
     #     print(entity)
     formatted_entities = format_entities(article_entities, article_text)
-    json_output += [{'article': article, 'entities': formatted_entities}]
+    json_output += [{"article": article, "entities": formatted_entities}]
     print(i)
-    #print('-' * 100)
+    # print('-' * 100)
     for entity in formatted_entities:
-        #print(entity)
+        # print(entity)
         # if isinstance(entity['entity'], list): print('mixed:', entity)
-        for score in entity['score']:
+        for score in entity["score"]:
             if score > 1:
-                print('failed:', entity)
+                print("failed:", entity)
                 exit()
 
-with jsonlines.open('data/new_results.jsonl', mode='w') as writer:
+with jsonlines.open("data/new_results.jsonl", mode="w") as writer:
     for article in json_output:
         writer.write(article)
 
-with jsonlines.open('data/new_failed.jsonl', mode='w') as writer:
+with jsonlines.open("data/new_failed.jsonl", mode="w") as writer:
     for article in failed_articles:
         writer.write(article)
